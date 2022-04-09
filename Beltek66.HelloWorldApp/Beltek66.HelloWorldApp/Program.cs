5520a3d1b8b885e41e50e811f7e058e6d258b0e8
unknown <artungokhan96@gmail.com>
2022-04-09 12:54:34 +0300

    init commit

---
+++ Beltek66.HelloWorldApp/Beltek66.HelloWorldApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Beltek66.HelloWorldApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim = "Ahmet";
+            int sayi = 5;
+            byte num = 3;
+
+            string[] cities = { "Ankara", "İstanbul" };
+            int[] notes = { 23, 12, 45, 78, 34 };
+
+            for (int i = 0; i < 10; i++) 
+            {
+                if ( i % 2 == 0 )
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
+            //Console.WriteLine(notes.Max());
+            //Console.WriteLine(notes.Min());
+            //Console.WriteLine(notes.Sum());
+
+            Console.ReadLine();
+        }
+        
+        static string runIt()
+        {
+            return "hello from runIt";
+        }
+    }
+}
