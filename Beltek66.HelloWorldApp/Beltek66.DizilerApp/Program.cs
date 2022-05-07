c8ef779142dd4f25b9479f0574ab7fbe21f071ac
unknown <artungokhan96@gmail.com>
2022-05-07 11:57:08 +0300

    aldkfjalkj

--- Beltek66.HelloWorldApp/Beltek66.DizilerApp/Program.cs
+++ Beltek66.HelloWorldApp/Beltek66.DizilerApp/Program.cs
@@ -7,64 +7,81 @@ namespace Beltek66.DizilerApp
         static void Main(string[] args)
         {
             #region array example - a simple example abt creating arrays and filling them
-            Console.WriteLine("Kaç adet sayı girmek istiyorsunuz?");
-            int totalNums = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Kaç adet sayı girmek istiyorsunuz?");
+            //int totalNums = int.Parse(Console.ReadLine());
 
-            int[] arr = new int[totalNums];
+            //int[] arr = new int[totalNums];
 
-            for (int i = 0; i < totalNums; i++)
-            {
-                Console.WriteLine($"\n{i + 1}. sayıyı giriniz:");
-                arr[i] = int.Parse(Console.ReadLine());
-            }
+            //for (int i = 0; i < totalNums; i++)
+            //{
+            //    Console.WriteLine($"\n{i + 1}. sayıyı giriniz:");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
 
-            Console.WriteLine("\nGirdiğiniz sayılar:");
-            foreach (int i in arr)
-            {
-                Console.WriteLine(i);
-            }
+            //Console.WriteLine("\nGirdiğiniz sayılar:");
+            //foreach (int i in arr)
+            //{
+            //    Console.WriteLine(i);
+            //}
             #endregion
 
-            Console.WriteLine("Kaç adet öğrenci bilgisi girilecek");
-            int numOfStudents = int.Parse(Console.ReadLine());
+            #region exam notes 
+            //Console.WriteLine("Kaç adet öğrenci bilgisi girilecek");
+            //int numOfStudents = int.Parse(Console.ReadLine());
 
-            string[,] students = new string[numOfStudents, 4];
+            //string[,] students = new string[numOfStudents + 1, 5];
+            //students[0, 0] = "Adı";
+            //students[0, 1] = "Soyadı";
+            //students[0, 2] = "Vize";
+            //students[0, 3] = "Final";
+            //students[0, 4] = "Ortalama";
 
-            for (int i = 0; i < numOfStudents; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    switch(j)
-                    {
-                        case 0:
-                            Console.WriteLine("Öğrenci adı:");
-                            string name = Console.ReadLine();
-                            students[i, j] = name;
-                            break;
-                        case 1:
-                            Console.WriteLine("Öğrenci soyadı:");
-                            string surname = Console.ReadLine();
-                            students[i, j] = surname;
-                            break;
-                        case 2:
-                            Console.WriteLine("Vize notu:");
-                            string midterm = Console.ReadLine();
-                            students[i, j] = midterm;
-                            break;
-                        case 3:
-                            Console.WriteLine("Final notu:");
-                            string final = Console.ReadLine();
-                            students[i, j] = final;
-                            break;
-                        
-                    }   
+            //for (int i = 1; i < students.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //        switch (j)
+            //        {
+            //            case 0:
+            //                Console.WriteLine($"{i}. Öğrenci adı:");
+            //                students[i, j] = Console.ReadLine();
+            //                break;
+            //            case 1:
+            //                Console.WriteLine($"{i}. Öğrenci soyadı:");
+            //                students[i, j] = Console.ReadLine();
+            //                break;
+            //            case 2:
+            //                Console.WriteLine($"{i}. Vize notu:");
+            //                students[i, j] = Console.ReadLine();
+            //                break;
+            //            case 3:
+            //                Console.WriteLine($"{i}. Final notu:");
+            //                students[i, j] = Console.ReadLine();
+            //                break;
+            //            case 4:
+            //                students[i, j] = Convert.ToString( (Convert.ToDouble(students[i, 2]) * 40 / 100) + (Convert.ToDouble(students[i, 3]) * 60 / 100) );
+            //                break;
+            //            default:    break;
+            //        }
+
                     
 
+            //    }
 
-                }
-                
-               
-            }
+
+            //}
+
+            //for(int i = 0; i < students.GetLength(0); i++)
+            //{
+            //    for(int j = 0; j < 5; j++)
+            //    {
+            //        Console.Write(students[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
         }
     }
 }
---
+++ Beltek66.HelloWorldApp/Beltek66.MethodsApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Beltek66.MethodsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("İlk sayıyı girin: \t");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("İkinci sayıyı girin: \t");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"\nsonuç: {Sum(num1, num2)}");
+
+            int[] nums = new int[] { 4, 2, 3 };
+            Console.WriteLine(nums.Sum());
+
+        }
+
+        static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        
+        static int Sum(int[] numArray)
+        {
+            int sum = 0;
+            foreach (int num in numArray)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
